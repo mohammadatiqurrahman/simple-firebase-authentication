ad3777891cab35df02a1afa31df9a197ad88a279
Mohammad Atiqur Rahman <atiqur15-6983@diu.edu.bd>
2021-10-07 12:07:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { getAuth,signInWithPopup,GoogleAuthProvider,GithubAuthProvider } from "firebase/auth";
+import { useState } from "react";
 
+import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+initializeAuthentication()
 function App() {
+  const [user,setUser] = useState([])
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+  const auth = getAuth();
+
+
+  const handleGoogleAuthentication=()=>{
+    signInWithPopup(auth, googleProvider)
+    .then(result=>{
+      const {displayName,email,photoURL} = result.user;
+      const loginInfo ={
+        name: displayName,
+        email: email,
+        photo: photoURL
+      }
+      setUser(loginInfo)
+    })
+  }
+
+  const handleGithubAuthentication=()=>{
+    signInWithPopup(auth, githubProvider)
+    .then(result=>{
+      const user = result.user;
+      console.log(user);
+    })
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleAuthentication}>Sign in With Google</button>
+      <button onClick={handleGithubAuthentication}>Sign in With Github</button>
+      {
+        <div>
+          <h6>{user.name}</h6>
+          <p>{user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvCJA1aa5DAxQMRnMXxMMJXmWlvgpqXTg",
+    authDomain: "simple-firebase-authenti-3609c.firebaseapp.com",
+    projectId: "simple-firebase-authenti-3609c",
+    storageBucket: "simple-firebase-authenti-3609c.appspot.com",
+    messagingSenderId: "404983068315",
+    appId: "1:404983068315:web:8a9f3aa39677de2d01383e"
+  };
+  export default firebaseConfig;
